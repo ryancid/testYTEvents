21d7a50abe940af247e0d1028d732d52182dbb11
ryanhsieh <ryancid0813@gmail.com>
2019-09-19 12:08:32 +0800

    Testing for not embeddable error

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'testYTEvents' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for testYTEvents
+  
+  pod 'YoutubePlayer-in-WKWebView', :git => 'https://github.com/ryancid/YoutubePlayer-in-WKWebView.git', :branch => 'onError'
+end
--- testYTEvents/ViewController.swift
+++ testYTEvents/ViewController.swift
@@ -7,14 +7,69 @@
 //
 
 import UIKit
+import YoutubePlayer_in_WKWebView
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerView: WKYTPlayerView!
+    @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        playerView.delegate = self
+        let privateVideoID = "KuxW_UIMVUY"
+        let notEmbeddableVideoID = "uRyAS_F3C20"
+        playerView.load(withVideoId: notEmbeddableVideoID, playerVars: [
+            "playsinline": 1,
+            "origin": "https://youtube.com",
+            "iv_load_policy": 3,
+            "modestbranding": 1,
+            "fs": 0
+            ])
+        
     }
 
 
 }
 
+extension ViewController: WKYTPlayerViewDelegate {
+    func playerView(_ playerView: WKYTPlayerView, didChangeTo state: WKYTPlayerState) {
+        switch state {
+        case .queued:
+            print("player queued")
+        case .playing:
+            print("player playing")
+        case .paused:
+            print("player paused")
+        case .ended:
+            print("player ended")
+        case .buffering:
+            print("player buffering")
+        case .unstarted:
+            print("player unstarted")
+        default:
+            print("player unknown state :\(state.rawValue)")
+            break
+        }
+    }
+    func playerView(_ playerView: WKYTPlayerView, receivedError error: WKYTPlayerError) {
+        print("player received error")
+        print(error)
+        var errorMessage = ""
+        switch error {
+        case .html5Error:
+            errorMessage = "HTML5 error"
+        case .invalidParam:
+            errorMessage = "Invalid Param"
+        case .notEmbeddable:
+            errorMessage = "Not Embeddable"
+        case .videoNotFound:
+            errorMessage = "Video Not Found"
+        case .unknown:
+            errorMessage = "Unknown"
+        default:
+            errorMessage = "Not Listed"
+        }
+        errorLabel.text = "Error: \(errorMessage)"
+    }
+}
